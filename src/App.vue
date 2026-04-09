32241af5b581a00ca8bf8a06d16a97ec4b4d7b31
Jeremy-Fowler <jembyfowler@gmail.com>
2026-04-09 14:30:09 -0600

    added auth 0

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue'
+
+const {
+  isLoading,
+  isAuthenticated,
+  error,
+  loginWithRedirect,
+  logout: auth0Logout,
+  user
+} = useAuth0()
+
+const signup = () =>
+  loginWithRedirect({ authorizationParams: { screen_hint: 'signup' } })
+
+const login = () => loginWithRedirect()
+
+const logout = () =>
+  auth0Logout({ logoutParams: { returnTo: window.location.origin } })
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <div v-if="isLoading">Loading...</div>
 
-<style scoped></style>
+  <div v-else-if="isAuthenticated && user">
+    <p>Logged in as {{ user.email }}</p>
+
+    <h1>User Profile</h1>
+
+    <pre>{{ JSON.stringify(user, null, 2) }}</pre>
+
+    <button @click="logout">Logout</button>
+  </div>
+
+  <div v-else>
+    <p v-if="error">Error: {{ error.message }}</p>
+
+    <button @click="signup">Signup</button>
+
+    <button @click="login">Login</button>
+  </div>
+</template>
